023a1b1f4a669ab5ace76d435d9acc3e7edef50f
stella maris yametti <stellayametti2027@gmail.com>
2021-03-09 20:08:16 -0300

    pueba stella

--- 2019_1Cuatri_Parcial_fuentes/ocho.js
+++ 2019_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,75 +1,81 @@
+/**Bienvenidos.
+Realizar el algoritmo que permita el ingreso por prompt de las alturas en centimetros(validar 
+entre 0 y 250) , el sexo. (validar el sexo “f” o “m”) de 5 jugadores de básquet, informar por alert:
+a) El promedio de las alturas totales.
+b) La altura más baja y el sexo de esa persona.
+c) La cantidad de muheres que su altura supere los 190 centimetros.*/
 
-/*Bienvenidos.
-Realizar el algoritmo que permita iterar el ingreso de dos datos, una letra y un número 
-entre -100 y 100 (validar) hasta que el usuario quiera e informar al terminar el ingreso por document.write:
-a) La cantidad de números pares.
-b) La cantidad de números impares.
-c) La cantidad de ceros.
-d) El promedio de todos los números positivos ingresados.
-e) La suma de todos los números negativos.
-f) El número y la letra del máximo y el mínimo.
+function mostrar() {
+	let contJugadores = 0;
+	let altura;
+	let sexoIngresado;
+	let sumAlturas = 0;
+	let promedioAlturas;
+	let banderaPuntoB;
+	let alturaMasBaja;
+	let sexoMasBaja;
+	let contMujeres190 = 0;
 
-Testeo con los siguientes datos
-( d , -150(mal) , -50(bien))
-( z , 0)
-( g , 20)
-( a , 150(mal) , 0(bien))
-( b , 3)
-( k , 7)
-Curso de ingreso UTN FRA*/
-function mostrar()
-{
-	var numeroIngresado;
-	var letraIngresada;
 
-	var contadorNumeroPar;
 
-	continuar="si";
-	contadorNumeroPar=0;
+	for (contJugadores = 0; contJugadores < 5; contJugadores++) {
 
-	while(continuar=="si")
 
-	{
+		altura = prompt("Ingrese la altura (0-250)");
+		altura = parseFloat(altura);
 
+		while (isNaN(altura) == true || altura < 0 || altura > 250) {
+			altura = prompt("Error# Ingrese la altura (0-250)");
+			altura = parseFloat(altura)
+		}
+
+		sexoIngresado = prompt("Ingrese el sexo f-m");
+
+		while (isNaN(sexoIngresado) == false || sexoIngresado != "f" && sexoIngresado != "m") {
+			sexoIngresado = prompt("Error# Ingrese el sexo f-m");
+		}
+
+		sumAlturas = sumAlturas + altura;
+
+		if (banderaPuntoB == 0 || altura < alturaMasBaja) {
+			alturaMasBaja = altura;
+			sexoMasBaja = sexoIngresado;
+
+			banderaPuntoB = 1;
+		}
+
+		if (altura > 190 && sexoIngresado == "f") {
+			contMujeres190++;
+
+		}
+
+
+
+
+
+
+	}//for
+	if (sumAlturas > 0) {
+		promedioAlturas = sumAltura / contJugadores;
+		console.log("el promedio de alturas es: " + promedioAlturas);
+	}
+	else {
+		console.log("no se ingresaron datos");
+	}
+
+	if (banderaPuntoB) {
+
+		console.log("la altura de la persona mas baja es: " + alturaMasBaja + "y su sexo es: " + sexoMasBaja);
+	}
+	else {
+		console.log("no se ingresaron datos");
+
+	}
+	if (contMujeres190>0) {
+		console.log("la cantidad de mujeres que supera el 190 son: "+contMujeres190);
 		
-	numeroIngresado=prompt("Ingrese la cantidad requerida");
-	numeroIngresado=parseInt(numeroIngresado);
-
-				while(isNaN(numeroIngresado)==true || numeroIngresado <-100 || numeroIngresado >100)
-				{
-				numeroIngresado=prompt("E# Ingrese una cantidad valida");
-				numeroIngresado=parseInt(numeroIngresado)
-				}
-
-	letraIngresada=prompt("Ingrese una letra");
-
-				while(isNaN(letraIngresada)==false )
-				{
-				letraIngresada=prompt("E# Ingrese una letra");
-				}
-
-
-	switch(numeroIngresado)
-			{
-			case(numeroIngresado%2==0):
-					contadorNumeroPar=contadorNumeroPar+1
-															
-			break;
-
-			case" ":		
-
-			break;
-
-			case" ":
-																	
-			break;
-			}
-
-
-				continuar=prompt("desea conitnuar ?");
-	}// fin while
-
-
-
-
-}
+	}
+	else{
+		console.log("no se ingresaron datos");
+	}
+}//ejercicio
